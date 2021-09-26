7aaa52f909267c4bb184e8314acc457c14af9779
Priya-sakthi <89513393+Priya-sakthi@users.noreply.github.com>
2021-09-26 15:31:23 +0530

    Add files via upload

---
+++ set operation.py
@@ -0,0 +1,18 @@
+# Program to perform different set operations
+# as we do in  mathematics
+
+# sets are define
+A = {2,5,6,8,9,-9}
+B = {4,7,3,8,2,1}
+
+# union
+print("Union :", A | B)
+
+# intersection
+print("Intersection :", A & B)
+
+# difference
+print("Difference :", A - B)
+
+# symmetric difference
+print("Symmetric difference :", A ^ B)
