9011b47b466924ea94966a778b34fe969774df04
Priya-sakthi <89513393+Priya-sakthi@users.noreply.github.com>
2021-09-26 15:30:47 +0530

    Update positive num.py

--- positive num.py
+++ positive num.py
@@ -1,5 +1,11 @@
 #printing posotive numbers in list
-list=[2,-8,6,-4,98,56,44]
+list=[]
+n=int(input("Enter the number of elements"))
+for in range(0,n)
+ele=int(input())
+list.append(ele)
+print(list)
 for n in  list:
     if n>0:
+        print("positive elements in list")
         print(n)
